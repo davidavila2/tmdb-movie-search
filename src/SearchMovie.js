6d02e295e369201c2b04e645ddf412b9cbd13ddc
david <david@venturplex.com>
2021-02-10 20:39:31 -0700

    incr changes

--- src/SearchMovie.js
+++ src/SearchMovie.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import MovieCard from "./MovieCard";
 
+const tmdbKey = process.env.REACT_APP_API_KEY;
+
 export default function SearchMovie() {
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
@@ -8,7 +10,7 @@ export default function SearchMovie() {
   const searchMovies = async (e) => {
     e.preventDefault();
 
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=39a0b634dc36eb022f43c09de2d7959f&language=en-US&query=${query}&page=1&include_adult=false`;
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${tmdbKey}f&language=en-US&query=${query}&page=1&include_adult=false`;
 
     try {
       const res = await fetch(url);
@@ -39,27 +41,6 @@ export default function SearchMovie() {
       </form>
       <div className="card-list">
         <MovieCard movies={movies} />
-        {/* {movies
-          .filter((movie) => movie.poster_path)
-          .map((movie) => (
-            <div className="card" key={movie.id}>
-              <img
-                className="card--image"
-                src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
-                alt={movie.title + " poster"}
-              />
-              <div className="card--content">
-                <h3 className="card--title">{movie.title}</h3>
-                <p>
-                  <small>RELEASE DATE: {movie.release_date}</small>
-                </p>
-                <p>
-                  <small>RATING: {movie.vote_average}</small>
-                </p>
-                <p className="card--desc">{movie.overview}</p>
-              </div>
-            </div>
-          ))} */}
       </div>
     </div>
   );
